73b9815034e25a6cdac92665320c2d10457cfedb
Jürgen Dufner <juergen@jdufner.de>
2024-05-16 10:19:39 +0200

    Initial Commit

---
+++ picturechoice/server/picturechoice_server.py
@@ -0,0 +1,42 @@
+import argparse
+from argparse import Namespace
+from datetime import datetime
+import json
+import logging
+import os
+
+
+class PictureChoiceServer:
+    def __init__(self, config: dict):
+        self.config = config
+
+    def run(self):
+        pass
+
+
+def __parse_arguments() -> Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str, help='A file containing all necessary config.')
+    return parser.parse_args()
+
+
+def __load_config() -> dict:
+    args: Namespace = __parse_arguments()
+    file = open(args.file)
+    conf: dict = json.load(file)
+    file.close()
+    return conf
+
+
+if __name__ == '__main__':
+    config: dict = __load_config()
+    now: datetime = datetime.now()
+    if not os.path.exists(config["logging"]["path"]):
+        os.makedirs(config["logging"]["path"])
+    logging.basicConfig(filename=f'{config["logging"]["path"]}/{now: %Y-%m-%d_%Hh%Mm%Ss}_main.log',
+                        encoding='utf-8',
+                        format='%(asctime)s,%(msecs)-3d - %(levelname)-8s - %(filename)s:%(lineno)d - '
+                               '%(module)s - %(funcName)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.getLevelNamesMapping()[config["logging"]["level"].upper()])
+    PictureChoiceServer(config).run()
